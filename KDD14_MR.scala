d1296980d28cde307aea97b9f2410903d2065c30
anadim <papailiopoulos@gmail.com>
2015-02-17 15:25:48 -0800

    Update KDD14_MR.scala

--- KDD14_MR.scala
+++ KDD14_MR.scala
@@ -3,6 +3,8 @@ import org.apache.spark.graphx.util._
 import org.apache.spark.graphx.lib._
 import org.apache.spark.rdd.RDD
 import scala.util.Random
+import scala.util.control.Breaks._
+
 
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
@@ -11,118 +13,82 @@ Logger.getLogger("org").setLevel(Level.WARN)
 Logger.getLogger("akka").setLevel(Level.WARN)
 
 
-// val vertexArray = Array(
-// 	(1L, (0)),
-// 	(2L, (0)),
-// 	(3L, (0)),
-// 	(4L, (0)),
-// 	(5L, (0))
-// 	)
-
-
-// val edgeArray = Array(
-// 	Edge(1, 2, 1),
-// 	Edge(1, 3, 1),
-// 	Edge(1, 5, 1),
-// 	Edge(2, 1, 1),
-// 	Edge(2, 4, 1),
-// 	Edge(2, 5, 1),
-// 	Edge(3, 1, 1),
-// 	Edge(3, 5, 1),
-// 	Edge(4, 2, 1),
-// 	Edge(5, 1, 1),
-// 	Edge(5, 2, 1),
-// 	Edge(5, 3, 1)
-// 	)
-
-// val vertexRDD: RDD[(Long,  (Int))] = sc.parallelize(vertexArray)
-// val edgeRDD: RDD[Edge[Int]] = sc.parallelize(edgeArray)
-// var graph: Graph[(Int), Int] = Graph(vertexRDD, edgeRDD)
-var graph = GraphGenerators.rmatGraph(sc, 100, 1000).mapVertices((vid, _) => 0)
-
+var graph: Graph[Int, Int] = GraphGenerators.logNormalGraph(sc, numVertices = 1000).mapVertices( (id, _) => -100.toInt )
 var unclusterGraph: Graph[(Int), Int] = graph
+var activeSubgraph: Graph[(Int), Int] = graph
 val epsilon: Double = 1
 
-val startTime = System.currentTimeMillis
 
-var iter = 0
+var x: Int = 1
+var prevRankGraph1: Graph[Int, Int] = null
+var prevRankGraph2: Graph[Int, Int] = null
+var maxDegree = graph.vertices.sample(false, 1, 1)	
+var clusterUpdates = graph.vertices.sample(false, 1, 1)	
+var maxDeg: Int = 0
+var randomSet = graph.vertices.sample(false, 1, 1)	
+var newVertices = graph.vertices.sample(false, 1, 1)	
+var hasFriends = graph.vertices.sample(false, 1, 1)	
 
-while (graph.vertices.filter(v => v._2 == 0).count()>0 && iter < 30) {
 
-	iter += 1
-
-	val time0 = System.currentTimeMillis
+while (graph.vertices.filter(v => v._2 == -100).count()>0) {
 	
-	unclusterGraph = graph.subgraph(vpred = (id, attr) => attr == 0)
-	System.out.println(s"unclusterGraph.vertices.count = ${unclusterGraph.vertices.count}")
-	System.out.println(s"graph.vertices.count          = ${graph.vertices.count}")
-	val time1 = System.currentTimeMillis
-
-	val maxDegree = unclusterGraph.aggregateMessages[Int](
-		triplet => { if (triplet.srcAttr == 0 & triplet.dstAttr == 0 ) {
+	 unclusterGraph = graph.subgraph(vpred = (id, attr) => attr == -100)
+	 maxDegree = unclusterGraph.aggregateMessages[Int](
+		triplet => { if (triplet.srcAttr == -100 & triplet.dstAttr == -100 ) {
 						triplet.sendToSrc(1) }
 						}, _ + _)
-	val time2 = System.currentTimeMillis
-
-
-	val maxDegInt = if (maxDegree.count == 0) 1 else maxDegree.toArray.map( x => x._2).max	
-	val randomSet = unclusterGraph.vertices.sample(false, epsilon/maxDegInt, scala.util.Random.nextInt(1000))	
-	System.out.println(s"randomSet.count = ${randomSet.count}")
-	val time3 = System.currentTimeMillis
-
-
+	maxDeg = if (maxDegree.count == 0) 1 else maxDegree.toArray.map( x => x._2).max	
+	randomSet = unclusterGraph.vertices.sample(false, math.min(epsilon/maxDeg,1), scala.util.Random.nextInt(1000))	
+	while(randomSet.count==0){
+		randomSet = unclusterGraph.vertices.sample(false, math.min(epsilon/maxDeg,1), scala.util.Random.nextInt(1000))	
+	}
+	
 
 	unclusterGraph = unclusterGraph.joinVertices(randomSet)((vId, attr, active) => -1)
-	val time4 = System.currentTimeMillis
-
-	// This is the extra part needed for the KDD14 paper
-	val activeSubgraph = unclusterGraph.subgraph(vpred = (id, attr) => attr == -1)
-	val time5 = System.currentTimeMillis
-
-	val hasFriends = activeSubgraph.degrees.filter{case (id, u) => u > 0}
-	val time6 = System.currentTimeMillis
-
+	activeSubgraph = unclusterGraph.subgraph(vpred = (id, attr) => attr == -1)
+	hasFriends = activeSubgraph.degrees.filter{case (id, u) => u > 0}
 	unclusterGraph = unclusterGraph.joinVertices(hasFriends)((vId, attr, active) => 0)
-	val time7 = System.currentTimeMillis
 
-	// extra code ends here
 
-	val clusterUpdates = unclusterGraph.aggregateMessages[Int](
+	// unclusterGraph = unclusterGraph.joinVertices(hasFriends)((vId, attr, active) => 0)
+	// unclusterGraph = unclusterGraph.joinVertices(randomSet)((vId, attr, active) => -1)
+	
+	clusterUpdates = unclusterGraph.aggregateMessages[Int](
 		triplet => {			
-			if ( triplet.dstAttr == 0 // if not clustered
+			if ( triplet.dstAttr == -100 // if not clustered
 				& triplet.srcAttr == -1 // the source is an active node
 				){ triplet.sendToDst(triplet.srcId.toInt) }
 			}, math.min(_ , _) 
 	)
-	val time8 = System.currentTimeMillis
-
-
-	val newVertices = unclusterGraph.vertices.leftJoin(clusterUpdates) {
+	newVertices = unclusterGraph.vertices.leftJoin(clusterUpdates) {
       (id, oldValue, newValue) =>
       newValue match {
 		  case Some(x:Int) => x 
 		  case None => {if (oldValue == -1) -10; else oldValue;}
      	}
-     }	
-	graph = graph.joinVertices(newVertices)((vId, oldAttr, newAttr) => newAttr)
-	val time9 = System.currentTimeMillis
+    }	
 
-	System.out.println(
-		s"Iter $iter,\t" +
-		s"${time1 - time0}\t" +
-		s"${time2 - time1}\t" +
-		s"${time3 - time2}\t" +
-		s"${time4 - time3}\t" +
-		s"${time5 - time4}\t" +
-		s"${time6 - time5}\t" +
-		s"${time7 - time6}\t" +
-		s"${time8 - time7}\t" +
-		s"${time9 - time8}\t" +
-		"")
+    graph = graph.joinVertices(newVertices)((vId, oldAttr, newAttr) => newAttr).cache()
+
+    // prevRankGraph1 = unclusterGraph
+    // prevRankGraph2 = graph
+
+    graph.edges.foreachPartition(x => {}) // also materializes rankGraph.vertices
+    // unclusterGraph.edges.foreachPartition(x => {}) // also materializes rankGraph.vertices
+    System.out.println(s"ClusterWild finished iteration $x.")
+    System.out.println(s"MaxDegree $maxDeg.")
+
+    // graph.vertices.unpersist(false)
+    // prevRankGraph2.vertices.unpersist(false)
+    // prevRankGraph1.edges.unpersist(false)
+    // prevRankGraph2.edges.unpersist(false)
+    x = x+1
+	// graph.vertices.collect()
+}
+System.out.println(s"ClusterIDs ${graph.vertices.collect().toList}.")
 
 
-} 
 
-val endTime = System.currentTimeMillis
 
-System.out.println(s"${endTime - startTime} ms taken")
+
+
