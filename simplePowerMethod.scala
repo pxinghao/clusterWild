5612fe1f8495c4247f8b671eccf93002eb92f0d4
Xinghao <pxinghao@gmail.com>
2015-02-20 11:10:59 -0800

    simplePowerMethod without prevRankGraph

--- simplePowerMethod.scala
+++ simplePowerMethod.scala
@@ -18,7 +18,7 @@ var rankGraph: Graph[Int, Int] = graph
 
 var iteration = 0
 var numIter = 10
-var prevRankGraph: Graph[Int, Int] = null
+// var prevRankGraph: Graph[Int, Int] = null
 
 while (iteration < numIter) {
     rankGraph.cache()
@@ -26,15 +26,15 @@ while (iteration < numIter) {
     val rankUpdates = rankGraph.aggregateMessages[Int](
         ctx => ctx.sendToDst(ctx.srcAttr), _ + _, TripletFields.Src)
 
-    prevRankGraph = rankGraph
+    // prevRankGraph = rankGraph
     rankGraph = rankGraph.joinVertices(rankUpdates) {
         (id, oldRank, msgSum) => msgSum
-    }
+    }.cache()
 
-    rankGraph.edges.foreachPartition(x => {}) // also materializes rankGraph.vertices
+    // rankGraph.edges.foreachPartition(x => {}) // also materializes rankGraph.vertices
     System.out.println(s"PowerMethod finished iteration $iteration.")
-    prevRankGraph.vertices.unpersist(false)
-    prevRankGraph.edges.unpersist(false)
+    // prevRankGraph.vertices.unpersist(false)
+    // prevRankGraph.edges.unpersist(false)
 
     iteration += 1
     
