72ee8ece9bb9a12c039653403767c18f27ea1f9a
Xinghao <pxinghao@gmail.com>
2015-03-10 18:32:30 -0700

    RDD for truncated lineage

--- src/main/scala/TruncatedLineageRDD.scala
+++ src/main/scala/TruncatedLineageRDD.scala
@@ -1,6 +1,18 @@
+import org.apache.spark.{TaskContext, Partition}
+import org.apache.spark.rdd.RDD
+
+import scala.reflect.ClassTag
+
 /**
  * Created by xinghao on 3/10/15.
  */
-class TruncatedLineageRDD {
 
+
+class TruncatedLineageRDD[T: ClassTag](rdd: RDD[T]) extends RDD[T](rdd.context, Nil) {
+  override def compute(split: Partition, context: TaskContext): Iterator[T] =
+    rdd.iterator(split, context)
+
+  override def getPartitions: Array[Partition] = rdd.partitions
+
+  override val partitioner = rdd.partitioner
 }
