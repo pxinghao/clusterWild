6fd2a2d5a5919072b8f92114b119f3b43b613e86
Xinghao <pxinghao@gmail.com>
2015-04-03 13:40:43 -0700

    Add caching of randomSet and clusterUpdates

--- src/main/scala/SimpleCheckpoint.scala
+++ src/main/scala/SimpleCheckpoint.scala
@@ -114,7 +114,7 @@ object SimpleCheckpoint {
 //      clusterGraph.vertices.cache().setName("v" + iteration + ".0")
 //      clusterGraph.edges.cache(   ).setName("e" + iteration + ".0")
 
-      val randomSet = clusterGraph.vertices.filter(v => (v._2 == initID) && (scala.util.Random.nextFloat < epsilon / maxDeg.toFloat))//.cache().setName("r" + iteration)
+      val randomSet = clusterGraph.vertices.filter(v => (v._2 == initID) && (scala.util.Random.nextFloat < epsilon / maxDeg.toFloat)).cache().setName("r" + iteration)
 //      if ((iteration+1) % checkpointIter == 0) randomSet.checkpoint()
       numNewCenters = randomSet.count
 
@@ -133,9 +133,10 @@ object SimpleCheckpoint {
             triplet.sendToDst(triplet.srcId.toInt)
           }
         }, math.min(_, _)
-      )//.cache().setName("u" + iteration)
+      ).cache().setName("u" + iteration)
 //      clusterUpdates.foreachPartition(_ => {})
 //      if ((iteration+1) % checkpointIter == 0) clusterUpdates.checkpoint()
+      System.out.println(s"#clusterUpdates = ${clusterUpdates.count()}")
 
 //      prevRankGraph = clusterGraph
       clusterGraph = clusterGraph.joinVertices(clusterUpdates) {
