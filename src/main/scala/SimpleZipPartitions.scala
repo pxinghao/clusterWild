953c528249e0b999b215dc0257f2e71d67225e96
Xinghao <pxinghao@gmail.com>
2015-04-06 22:14:28 -0700

    Simplify SimpleZipPartitions

--- src/main/scala/SimpleZipPartitions.scala
+++ src/main/scala/SimpleZipPartitions.scala
@@ -1,20 +1,10 @@
-import org.apache.log4j.{Level, Logger}
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.graphx._
-import org.apache.spark.graphx.util.GraphGenerators
+import org.apache.spark.SparkContext
 import org.apache.spark.rdd._
-import org.apache.spark.SparkContext._
-
-import scala.collection.immutable.Map
-import scala.sys.process._
 
 object SimpleZipPartitions{
   def main(args: Array[String]) = {
 
     val sc = new SparkContext()
-
-    Logger.getLogger("org").setLevel(Level.WARN)
-    Logger.getLogger("akka").setLevel(Level.WARN)
 
     val argmap: Map[String, String] = args.map { a =>
       val argPair = a.split("=")
@@ -27,14 +17,18 @@ object SimpleZipPartitions{
     val checkpointIter  : Int     = argmap.getOrElse("checkpointiter", "20").toInt
     val checkpointDir   : String  = argmap.getOrElse("checkpointdir", "/mnt/checkpoints/")
 
-    var R : RDD[(Long,Int)] = sc.parallelize((0 until numPartitions), numPartitions).mapPartitions(_ => new Array[(Long,Int)](10000000).toSeq.iterator).cache()
+    var R    : RDD[(Long,Int)] = sc.parallelize((0 until numPartitions), numPartitions).mapPartitions(_ => new Array[(Long,Int)](10000000).toSeq.iterator).cache()
+    var oldR : RDD[(Long,Int)] = null
+
+    sc.setCheckpointDir(checkpointDir)
 
     var iteration = 0
     while (iteration < 50){
-      R = R.zipPartitions(R) { (x,y) => x }.cache().setName("r" + iteration)
-      if ((iteration+1) % checkpointIter == 0) sc.setCheckpointDir(checkpointDir + iteration.toString)
+      oldR = R
+      R = R.zipPartitions(R) { (x,y) => x }.cache()
       if ((iteration+1) % checkpointIter == 0) R.checkpoint()
       R.foreachPartition(_ => {})
+      oldR.unpersist()
       iteration += 1
     }
 
