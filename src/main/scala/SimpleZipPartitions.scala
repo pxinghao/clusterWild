8ee22b99753d082b59b49bef405179a7e79e899d
Xinghao <pxinghao@gmail.com>
2015-04-06 23:30:32 -0700

    zipPartitions

--- src/main/scala/SimpleZipPartitions.scala
+++ src/main/scala/SimpleZipPartitions.scala
@@ -15,14 +15,13 @@ object SimpleZipPartitions{
     var R : RDD[(Long,Int)]
     = sc.parallelize((0 until numPartitions), numPartitions)
       .mapPartitions(_ => new Array[(Long,Int)](10000000).map(i => (0L,0)).toSeq.iterator).cache()
-//      .mapPartitions(_ => new Array[(Long,Int)](10000000).toSeq.iterator).cache()
 
     sc.setCheckpointDir(checkpointDir)
 
     var iteration = 0
     while (iteration < 50){
-      R = R.join(R).map(_ => (0L,0)).cache()
-//      R = R.zipPartitions(R)((x,y) => x).cache()
+//      R = R.join(R).map(_ => (0L,0)).cache()
+      R = R.zipPartitions(R)((x,y) => x).cache()
       if ((iteration+1) % checkpointIter == 0) R.checkpoint()
       R.foreachPartition(_ => {})
       iteration += 1
