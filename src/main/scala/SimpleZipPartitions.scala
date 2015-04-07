0e96a08161b31ebcee7f99f1b72ef47b08ce5bac
Xinghao <pxinghao@gmail.com>
2015-04-06 20:54:30 -0700

    Set SparkConfs

--- src/main/scala/SimpleZipPartitions.scala
+++ src/main/scala/SimpleZipPartitions.scala
@@ -1,12 +1,31 @@
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.SparkContext
+import org.apache.spark.{SparkConf, SparkContext}
 
 object SimpleZipPartitions{
   def main(args: Array[String]) = {
     Logger.getLogger("org").setLevel(Level.WARN)
     Logger.getLogger("akka").setLevel(Level.WARN)
 
-    val sc = new SparkContext()
+    System.setProperty("spark.worker.timeout",                     "30000")
+    System.setProperty("spark.storage.blockManagerHeartBeatMs",    "5000")
+    System.setProperty("spark.storage.blockManagerSlaveTimeoutMs", "100000")
+    System.setProperty("spark.akka.timeout",                       "30000")
+    System.setProperty("spark.akka.retry.wait",                    "30000")
+    System.setProperty("spark.akka.frameSize",                     "2047")
+    System.setProperty("spark.locality.wait",                      "300000000000")
+    val sc = new SparkContext(new SparkConf().setAll(List[(String,String)](
+      ("spark.worker.timeout",                     "30000"),
+      ("spark.storage.blockManagerHeartBeatMs",    "5000"),
+      ("spark.storage.blockManagerSlaveTimeoutMs", "100000"),
+      ("spark.akka.timeout",                       "30000"),
+      ("spark.akka.retry.wait",                    "30000"),
+      ("spark.akka.frameSize",                     "2047"),
+      ("spark.locality.wait",                      "300000000000"),
+      ("spark.logConf",                            "true")
+    )))
+
+    Logger.getLogger("org").setLevel(Level.WARN)
+    Logger.getLogger("akka").setLevel(Level.WARN)
 
     val checkpointDir   : String  = "/mnt/checkpoints/"
     sc.setCheckpointDir(checkpointDir)
