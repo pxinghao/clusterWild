8c0c646d5899dbd5fc8f7a58d46b1c89eb846587
Xinghao <pxinghao@gmail.com>
2015-04-06 20:57:55 -0700

    Add graph loading

--- src/main/scala/SimpleZipPartitions.scala
+++ src/main/scala/SimpleZipPartitions.scala
@@ -1,5 +1,9 @@
 import org.apache.log4j.{Level, Logger}
+import org.apache.spark.graphx.{GraphLoader, Graph}
+import org.apache.spark.graphx.util.GraphGenerators
 import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.collection.immutable.Map
 
 object SimpleZipPartitions{
   def main(args: Array[String]) = {
@@ -27,7 +31,49 @@ object SimpleZipPartitions{
     Logger.getLogger("org").setLevel(Level.WARN)
     Logger.getLogger("akka").setLevel(Level.WARN)
 
-    val checkpointDir   : String  = "/mnt/checkpoints/"
+    val argmap: Map[String, String] = args.map { a =>
+      val argPair = a.split("=")
+      val name = argPair(0).toLowerCase
+      val value = argPair(1)
+      (name, value)
+    }.toMap
+
+    val graphType       : String  = argmap.getOrElse("graphtype", "rmat").toLowerCase
+    val rMatNumEdges    : Int     = argmap.getOrElse("rmatnumedges", "100000000").toInt
+    val path            : String  = argmap.getOrElse("path", "graphs/astro.edges")
+    val numPartitions   : Int     = argmap.getOrElse("numpartitions", "640").toInt
+    val epsilon         : Double  = argmap.getOrElse("epsilon", "0.5").toDouble
+    val checkpointIter  : Int     = argmap.getOrElse("checkpointiter", "20").toInt
+    val checkpointDir   : String  = argmap.getOrElse("checkpointdir", "/mnt/checkpoints/")
+    val checkpointClean : Boolean = argmap.getOrElse("checkpointclean", "true").toBoolean
+    val checkpointLocal : Boolean = argmap.getOrElse("checkpointlocal", "false").toBoolean
+
+    System.out.println(s"graphType      = $graphType")
+    System.out.println(s"rMatNumEdges   = $rMatNumEdges")
+    System.out.println(s"path           = $path")
+    System.out.println(s"numPartitions  = $numPartitions")
+    System.out.println(s"epsilon        = $epsilon")
+    System.out.println(s"checkpointIter = $checkpointIter")
+
+    val initID: Int = -100
+    val centerID: Int = -200
+
+    val graph: Graph[(Int), Int] =
+      if (graphType == "rmat")
+        GraphGenerators.rmatGraph(sc, requestedNumVertices = rMatNumEdges.toInt, numEdges = rMatNumEdges.toInt).mapVertices((id, _) => initID.toInt)
+      else
+        GraphLoader.edgeListFile(sc, path, false, numPartitions)
+
+    val numVertices = graph.vertices.count
+    val logN = math.floor(math.log(numVertices.toDouble))
+    val maxDegRecomputeRounds = logN //math.floor(2.0 / epsilon * logN.toDouble)
+
+    System.out.println(
+      s"Graph has $numVertices vertices (${graph.vertices.partitions.length} partitions),"
+        + s"${graph.edges.count} edges (${graph.edges.partitions.length} partitions),"
+        + s"eps = $epsilon"
+    )
+
     sc.setCheckpointDir(checkpointDir)
 
     val numRands = 10000
